3d12a6c3a00ebaf0deb00d0abb0902c12480bd4e
Oded2 <111157775+Oded2@users.noreply.github.com>
2025-06-03 13:26:44 +0300

    Refactor sign-up state declaration in Auth page; change variable from let to const for improved clarity

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -2,7 +2,7 @@
 	import Auth from '$lib/components/Auth.svelte';
 	import { page } from '$app/state';
 
-	let signUp = $derived(page.url.searchParams.get('action') !== 'login');
+	const signUp = $derived(page.url.searchParams.get('action') !== 'login');
 </script>
 
 <Auth initialSignUp={signUp}></Auth>
