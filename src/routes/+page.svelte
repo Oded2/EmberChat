2cd2f9d886c41c17b39bb07370c10fe25dca06c0
Oded2 <111157775+Oded2@users.noreply.github.com>
2025-06-06 01:37:18 +0300

    Update layout styles for responsiveness; adjust flex properties and center button and image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="flex flex-col gap-8 md:flex-row">
+<div class="flex flex-col gap-8 lg:flex-row">
 	<div class="flex grow flex-col gap-2">
 		<h1 class="text-8xl font-bold">Zero complexity, just chat.</h1>
 		<p class="text-lg font-light">
@@ -6,7 +6,7 @@
 			laboriosam fugiat vitae architecto nobis esse, nisi consectetur ipsam atque minus placeat
 			aperiam quis in maiores assumenda omnis?
 		</p>
-		<a href="/chat" class="btn btn-primary btn-wide mt-2">Begin</a>
+		<a href="/chat" class="btn btn-primary btn-wide mx-auto mt-2 md:mx-0">Begin</a>
 	</div>
-	<img src="/favicon.png" alt="Logo" class="max-w-md rounded-2xl shadow-2xl" />
+	<img src="/favicon.png" alt="Logo" class="mx-auto max-w-md rounded-2xl shadow-2xl" />
 </div>
