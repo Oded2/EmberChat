14fa5396ec7eaa2d3308024f09afbe22f538cd85
Oded2 <111157775+Oded2@users.noreply.github.com>
2025-06-07 12:42:37 +0300

    Refactor LabelTextarea component to improve auto-resizing and handle Enter key press for better user interaction

--- src/lib/components/LabelTextarea.svelte
+++ src/lib/components/LabelTextarea.svelte
@@ -3,35 +3,37 @@
 
 	interface Props {
 		label?: string;
-		value?: string;
+		value: string;
 		required?: boolean;
 		children: Snippet;
 	}
 
 	let { label, value = $bindable(), required, children }: Props = $props();
 
+	let textarea: HTMLTextAreaElement;
 	let hiddenSubmit: HTMLButtonElement;
 
-	function autoResize(textarea: HTMLTextAreaElement) {
-		textarea.style.height = 'auto'; // Reset height
-		textarea.style.height = textarea.scrollHeight + 'px'; // Set to scroll height
-	}
-
-	function handleKeyDown(event: KeyboardEvent) {
-		if (event.key === 'Enter' && !event.shiftKey) {
-			event.preventDefault(); // Prevent new line
-			hiddenSubmit.click();
+	$effect(() => {
+		// Run on value change
+		if (value.length >= 0) {
+			textarea.style.height = 'auto'; // Reset height
+			textarea.style.height = textarea.scrollHeight + 'px'; // Set to scroll height
 		}
-	}
+	});
 </script>
 
 <div class="bg-base-100 flex w-full flex-col rounded-2xl px-3 pt-1 pb-3">
 	<textarea
+		bind:this={textarea}
 		bind:value
 		class="bg-base-100 flex max-h-40 w-full grow resize-none gap-2 rounded-2xl px-4 py-3 outline-none"
 		placeholder={label}
-		oninput={(e) => autoResize(e.currentTarget)}
-		onkeydown={(e) => handleKeyDown(e)}
+		onkeydown={(e) => {
+			if (e.key === 'Enter' && !e.shiftKey) {
+				e.preventDefault(); // Prevent new line
+				hiddenSubmit.click();
+			}
+		}}
 		rows="1"
 		{required}
 	></textarea>
