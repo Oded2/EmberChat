562c7dec414b2e040895c90ab84c03fcfc98e925
Oded2 <111157775+Oded2@users.noreply.github.com>
2025-06-07 12:33:20 +0300

    Add hidden submit button and handle Enter key press in LabelTextarea component for improved user interaction

--- src/lib/components/LabelTextarea.svelte
+++ src/lib/components/LabelTextarea.svelte
@@ -10,9 +10,18 @@
 
 	let { label, value = $bindable(), required, children }: Props = $props();
 
+	let hiddenSubmit: HTMLButtonElement;
+
 	function autoResize(textarea: HTMLTextAreaElement) {
 		textarea.style.height = 'auto'; // Reset height
 		textarea.style.height = textarea.scrollHeight + 'px'; // Set to scroll height
+	}
+
+	function handleKeyDown(event: KeyboardEvent) {
+		if (event.key === 'Enter' && !event.shiftKey) {
+			event.preventDefault(); // Prevent new line
+			hiddenSubmit.click();
+		}
 	}
 </script>
 
@@ -22,8 +31,10 @@
 		class="bg-base-100 flex max-h-40 w-full grow resize-none gap-2 rounded-2xl px-4 py-3 outline-none"
 		placeholder={label}
 		oninput={(e) => autoResize(e.currentTarget)}
+		onkeydown={(e) => handleKeyDown(e)}
 		rows="1"
 		{required}
 	></textarea>
 	{@render children()}
 </div>
+<button bind:this={hiddenSubmit} class="hidden">Hidden Submit</button>
