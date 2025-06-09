9436b62b83cd8427e2414c1ff39a55fa140c7eb6
Oded2 <111157775+Oded2@users.noreply.github.com>
2025-06-09 14:51:42 +0300

    Update Chat component to ensure full-width layout for message sender details

--- src/lib/components/Chat.svelte
+++ src/lib/components/Chat.svelte
@@ -115,7 +115,7 @@
 				animate:flip={{ duration: 200 }}
 				class="bg-base-100 group flex justify-between gap-2 rounded-lg px-4 py-2"
 			>
-				<div class="flex flex-col">
+				<div class="flex w-full flex-col" dir="auto">
 					<div class="flex items-baseline gap-x-1.5">
 						<span class="font-semibold">{message.senderName}</span>
 						<span class="text-xs font-light">
