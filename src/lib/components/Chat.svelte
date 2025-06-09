c7cffe0d275dd186c09ba3d545de513bd905f8c3
Oded2 <111157775+Oded2@users.noreply.github.com>
2025-06-09 12:38:39 +0300

    Refactor Chat component to improve spacing between sender name and timestamp

--- src/lib/components/Chat.svelte
+++ src/lib/components/Chat.svelte
@@ -114,9 +114,9 @@
 				class="bg-base-100 flex justify-between gap-2 rounded-lg px-4 py-2"
 			>
 				<div class="flex flex-col">
-					<div class="flex items-baseline gap-2">
+					<div class="flex items-baseline gap-x-1.5">
 						<span class="font-semibold">{message.senderName}</span>
-						<span class="text-xs font-light italic">
+						<span class="text-xs font-light">
 							{message.timestamp.toLocaleString(undefined, {
 								minute: 'numeric',
 								hour: 'numeric',
