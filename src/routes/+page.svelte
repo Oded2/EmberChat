f7c6e3bddf0bb25bb1be10d28879a157c6550f5a
Oded2 <111157775+Oded2@users.noreply.github.com>
2025-06-15 01:27:34 +0300

    Refactor room code input; replace LabelInputForm with LabelInput and streamline button layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,10 @@
 	import { goto } from '$app/navigation';
 	import DynamicSpinner from '$lib/components/DynamicSpinner.svelte';
 	import LabelInput from '$lib/components/LabelInput.svelte';
-	import LabelInputForm from '$lib/components/LabelInputForm.svelte';
 	import Title from '$lib/components/Title.svelte';
 	import { alphanumericRegex, getRandomInt, globalRoomCode } from '$lib/helpers';
 	import { t } from '$lib/stores/localization';
 	import { addToast } from '$lib/stores/toasts';
-	import { get } from 'svelte/store';
 
 	let roomCode = $state('');
 	let inProgress = $state(false);
@@ -41,33 +39,30 @@
 	<div class="flex grow flex-col gap-4">
 		<h1 class="text-6xl font-bold md:text-8xl">{$t('heading')}</h1>
 		<p class="text-lg font-light">{$t('description')}</p>
-		<a href="/chat" class="btn btn-primary btn-lg btn-wide mx-auto mt-2">
+		<a href="/chat" class="btn btn-primary btn-lg sm:btn-wide mt-2 sm:mx-auto">
 			<i class="fa-solid fa-globe"></i>
 			{$t('enter_global')}
 		</a>
 		<div class="divider"><span class="font-medium">{$t('or')}</span></div>
 		<h4 class="text-2xl font-semibold">{$t('enter_chat_room')}</h4>
-		<div class="sm:me-auto sm:min-w-xl">
-			<LabelInputForm handleSubmit={goToRoom}>
-				<LabelInput
-					bind:value={roomCode}
-					label={$t('room_code')}
-					autocorrect="off"
-					autocapitalize="off"
-					spellcheck={false}
-				></LabelInput>
-				<button
-					type="button"
-					onclick={generateRoomCode}
-					class="btn btn-primary btn-outline btn-lg w-16"
-					aria-label="Random"
-				>
-					<i class="fa-solid fa-dice"></i>
-				</button>
-				<button type="submit" class="btn btn-primary btn-lg w-16">
-					<DynamicSpinner text={$t('go')} {inProgress}></DynamicSpinner>
-				</button>
-			</LabelInputForm>
+		<div class="flex flex-wrap gap-2 sm:me-auto sm:min-w-xl">
+			<LabelInput
+				bind:value={roomCode}
+				label={$t('room_code')}
+				autocorrect="off"
+				autocapitalize="off"
+				spellcheck={false}
+			></LabelInput>
+			<button
+				onclick={generateRoomCode}
+				class="btn btn-primary btn-outline btn-lg sm:w-16"
+				aria-label="Random"
+			>
+				<i class="fa-solid fa-dice"></i>
+			</button>
+			<button onclick={goToRoom} class="btn btn-primary btn-lg w-full sm:w-16">
+				<DynamicSpinner text={$t('go')} {inProgress}></DynamicSpinner>
+			</button>
 		</div>
 		<span class="text-sm font-light italic before:me-0.5 before:content-['*']">
 			{$t('room_code_pattern')}
