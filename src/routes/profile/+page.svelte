4083ca70bdaaf3d43f79f54d7fb33a4f3aa633c6
Oded2 <111157775+Oded2@users.noreply.github.com>
2025-06-16 19:38:26 +0300

    Update layout of profile tabs to support responsive design

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -19,7 +19,7 @@
 			<h1 class="mb-10 text-center text-3xl font-bold">
 				{$t('personal_greeting').replace('%NAME%', userData.displayName ?? '')}
 			</h1>
-			<div class="tabs tabs-box mx-auto mb-2">
+			<div class="tabs tabs-box mx-auto mb-2 flex-col sm:flex-row">
 				<ProfileTab label={$t('settings')} checked onchange={() => (tab = 0)}></ProfileTab>
 				<ProfileTab
 					label={$t('stats')}
